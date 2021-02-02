f40322fb05628bc06df2bf841369680c9fbd13fd
John Gomes <55712509+johnnathanielgomes@users.noreply.github.com>
2021-02-02 00:16:46 -0500

    added average and std

--- solutions.py
+++ solutions.py
@@ -8,6 +8,7 @@ import datetime
 data = pd.read_csv("data/DataSample.csv")
 data.sort_values(by= ['Latitude', ' TimeSt'], inplace=True, ignore_index=True)
 filtered = data.copy()
+print(filtered.shape)
 
 for index, row in data.iterrows():
     try:
@@ -20,6 +21,8 @@ for index, row in data.iterrows():
         if date == next_date:
             filtered.drop(index=index, inplace=True)
             filtered.drop(index=index+1, inplace=True)
+
+print(filtered.shape)
 
 # Label
 
@@ -45,3 +48,36 @@ for index, row in filtered.iterrows():
     filtered.at[index, 'ClosestPOI'] = min_poi
     filtered.at[index, 'DistancePOI'] = min_distance
 
+print("Labels added")
+
+# Analysis Part 1
+
+poi['Total'] = 0.0
+poi['Average'] = 0.0
+poi['Std'] = 0.0
+
+for index, row in filtered.iterrows():
+    poi_index = poi[poi['POIID'] == row['ClosestPOI']].index.values.astype(int)[0]
+    poi.at[poi_index, 'Total'] = poi.at[poi_index, 'Total'] + 1
+    poi.at[poi_index, 'Average'] = poi.at[poi_index, 'Average'] + row['DistancePOI']
+
+for index, row in poi.iterrows():
+    if poi.at[index, 'Total'] == 0:
+        continue
+    else:
+        poi.at[index, 'Average'] = poi.at[index, 'Average'] / poi.at[index, 'Total']
+
+for index, row in filtered.iterrows():
+    poi_index = poi[poi['POIID'] == row['ClosestPOI']].index.values.astype(int)[0]
+    poi.at[poi_index, 'Std'] = poi.at[poi_index, 'Std'] + ((row['DistancePOI'] - poi.at[poi_index, 'Average'])**2)
+
+for index, row in poi.iterrows():
+    if poi.at[index, 'Total'] == 0:
+        continue
+    else:
+        poi.at[index, 'Std'] = poi.at[index, 'Std'] / (poi.at[index, 'Total'] - 1)
+        poi.at[index, 'Std'] = math.sqrt(poi.at[index, 'Std'])
+
+for index, row in poi.iterrows():
+    print(str(row['POIID']) + " average distance: " + str(row['Average']))
+    print(str(row['POIID']) + " standard deviation: " + str(row['Std']))
